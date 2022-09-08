33661f587e78ab0994765c6538625c6fcc46f3ef
KubrakSasha <100533977+KubrakSasha@users.noreply.github.com>
2022-09-08 21:26:47 +0300

    PingPong DONE

--- My project (1)/Assets/Scripts/PingPong.cs
+++ My project (1)/Assets/Scripts/PingPong.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PingPong : MonoBehaviour
-{    
-    public float speed = 0.1f;
-    public float pos = 5;
-    // Start is called before the first frame update
+{
+    public Vector3 destination;
+   
     void Start()
     {
         
@@ -14,15 +13,7 @@ public class PingPong : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0f, 0f, speed) * Time.deltaTime);
-        float posX = transform.position.z;
-        if (posX > pos)
-        {
-            speed = -speed;            
-        }
-        if (posX < 0 )
-        {
-            speed = -speed;
-        }
+        transform.position = Vector3.Lerp(Vector3.zero, destination, Mathf.PingPong(Time.time, 1f));
+       
     }
 }
