004d17db2e18466cbdf8ffa6e95166b3e1f4f9f7
KubrakSasha <100533977+KubrakSasha@users.noreply.github.com>
2022-11-02 16:11:40 +0300

    Add BulletMark

--- Homework04/Assets/Scripts/Ammo/Bullet.cs
+++ Homework04/Assets/Scripts/Ammo/Bullet.cs
@@ -4,17 +4,21 @@ public class Bullet : Ammo
 {
     [SerializeField] GameObject bulletCollisionPrefab;
     [SerializeField] GameObject bulletTrailPrefab;
+    [SerializeField] GameObject bulletMarkPrefab;
+
     float velocity = 30f;
 
     public override void Fire(Vector3 robotShootDirection)
     {
-        Instantiate(bulletTrailPrefab, transform.position, Quaternion.identity, this.transform);
+        //Instantiate(bulletTrailPrefab, transform.position, Quaternion.identity, this.transform);
         this.gameObject.GetComponent<Rigidbody>().AddForce(robotShootDirection * velocity, ForceMode.Impulse);
     }
     private void OnCollisionEnter(Collision collision)
     {
         var effect = Instantiate(bulletCollisionPrefab, collision.contacts[0].point, Quaternion.identity) as GameObject;
+        var bulletMark = Instantiate(bulletMarkPrefab, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal));
         Destroy(this.gameObject);
         Destroy(effect, 0.5f);// c GameObject работает, а с ParticleSystem не работает
+        Destroy(bulletMark, 20f);
     }
 }
