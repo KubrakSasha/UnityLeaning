b687c24b1f3e6b75e1a22472e0b7adc61518bf39
KubrakSasha <100533977+KubrakSasha@users.noreply.github.com>
2022-11-03 12:42:15 +0300

    Door open fixed

--- Homework02_02/Assets/Scripts/OpenDoor.cs
+++ Homework02_02/Assets/Scripts/OpenDoor.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
 public class OpenDoor : MonoBehaviour
-{
-    [SerializeField] float smooth;
+{   
     [SerializeField] float doorOpenAngle = 90;
     [SerializeField] GameObject door;
 
     Vector3 closeRotation;
-    Vector3 openRotation;   
+    Vector3 openRotation;
     void Start()
     {
         closeRotation = door.transform.eulerAngles;
-        openRotation = new Vector3(closeRotation.x, closeRotation.y + doorOpenAngle, closeRotation.z);        
-    }   
+        openRotation = new Vector3(closeRotation.x, closeRotation.y + doorOpenAngle, closeRotation.z);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        door.transform.eulerAngles = Vector3.Lerp(openRotation, closeRotation, Time.deltaTime * smooth);      
-    }   
+        door.transform.eulerAngles += openRotation;           
+    }
     private void OnTriggerExit(Collider other)
     {
-        door.transform.eulerAngles = Vector3.Lerp(closeRotation, openRotation, Time.deltaTime * smooth);       
+        door.transform.eulerAngles -= openRotation;       
     }
 }
 
